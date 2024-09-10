df794bab860c3cb422077f99bec9b4473835e88f
SerifeGokdas1 <serifegokdaas@gmail.com>
2024-09-10 13:36:15 +0300

    Add project files.

---
+++ EasyProblems/RunningSumProblem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmChallenges.EasyProblems
+{
+    //Problem:
+    //Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+    //Return the running sum of nums.
+    public class RunningSumProblem
+    {
+        public static int[] RunningSum(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+            result[0] = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                result[i] = result[i - 1] + nums[i];
+            }
+            return result;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+#region EasyProblems
+
+#region RunningSum
+using AlgorithmChallenges.EasyProblems;
+
+int[] nums = { 1, 2, 3, 4 };
+int[] result = RunningSumProblem.RunningSum(nums);
+
+Console.WriteLine("Running Sum: " + string.Join(", ", result));
+#endregion
+
+
+#endregion
+
+
+
+
+
