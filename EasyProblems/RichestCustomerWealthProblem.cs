baf170fa58039476b6b5d30325ce863cd0924080
SerifeGokdas1 <serifegokdaas@gmail.com>
2024-09-10 15:01:41 +0300

    Resolved issue with MaximumWealth

---
+++ EasyProblems/RichestCustomerWealthProblem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmChallenges.EasyProblems
+{
+    public class RichestCustomerWealthProblem
+    {
+        //Problem
+        //You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+        //A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+
+        public static int MaximumWealth(int[][] accounts)
+        {
+            int wealth = 0;
+
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                int money = 0;
+                for (int y = 0; y < accounts[i].Length; y++)
+                {
+                    money += +accounts[i][y];
+                }
+                wealth = (wealth < money) ? money : wealth;
+            }
+            return wealth;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,27 @@
 ﻿
 #region EasyProblems
 
-#region RunningSum
+//#region RunningSum
+//using AlgorithmChallenges.EasyProblems;
+
+//int[] nums = { 1, 2, 3, 4 };
+//int[] result = RunningSumProblem.RunningSum(nums);
+
+//Console.WriteLine("Running Sum: " + string.Join(", ", result));
+//#endregion
+
+#region RichestCustomerWealth
+
 using AlgorithmChallenges.EasyProblems;
 
-int[] nums = { 1, 2, 3, 4 };
-int[] result = RunningSumProblem.RunningSum(nums);
-
-Console.WriteLine("Running Sum: " + string.Join(", ", result));
+int[][] accounts = {
+    new int[] {1, 2, 3},
+    new int[] {4, 5, 6},
+    new int[] {7, 8, 9}
+        };
+int result = RichestCustomerWealthProblem.MaximumWealth(accounts);
+Console.WriteLine("Richest Customer Wealth: " + string.Join(", ", result));
 #endregion
-
 
 #endregion
 
