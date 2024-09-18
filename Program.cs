50fc4caaaf37df4ceddf81c1caa96a56c06ce637
SerifeGokdas1 <serifegokdaas@gmail.com>
2024-09-18 11:03:44 +0300

    The FizzBuzz problem has been solved.

--- Program.cs
+++ Program.cs
@@ -10,17 +10,29 @@
 //Console.WriteLine("Running Sum: " + string.Join(", ", result));
 //#endregion
 
-#region RichestCustomerWealth
+//#region RichestCustomerWealth
 
+//using AlgorithmChallenges.EasyProblems;
+
+//int[][] accounts = {
+//    new int[] {1, 2, 3},
+//    new int[] {4, 5, 6},
+//    new int[] {7, 8, 9}
+//        };
+//int result = RichestCustomerWealthProblem.MaximumWealth(accounts);
+//Console.WriteLine("Richest Customer Wealth: " + string.Join(", ", result));
+//#endregion
+
+#region  FizzBuzz
 using AlgorithmChallenges.EasyProblems;
 
-int[][] accounts = {
-    new int[] {1, 2, 3},
-    new int[] {4, 5, 6},
-    new int[] {7, 8, 9}
-        };
-int result = RichestCustomerWealthProblem.MaximumWealth(accounts);
-Console.WriteLine("Richest Customer Wealth: " + string.Join(", ", result));
+int n = 15; 
+var result = FizzBuzzProblem.FizzBuzz(n);
+
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
 #endregion
 
 #endregion
