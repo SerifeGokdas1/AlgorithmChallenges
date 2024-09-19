5e33dfc7c7b841b266f167339cfe764bb7dbd030
SerifeGokdas1 <serifegokdaas@gmail.com>
2024-09-19 17:28:54 +0300

    Numbers Of Steps Reduce problem has been solved.

--- EasyProblems/NumberofStepstoReduceaNumbertoZeroProblem.cs
+++ EasyProblems/NumberofStepstoReduceaNumbertoZeroProblem.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace AlgorithmChallenges.EasyProblems
 {
-    internal class NumberofStepstoReduceaNumbertoZeroProblem
+    public class NumberofStepstoReduceaNumbertoZeroProblem
     {
+        public static int NumberOfSteps(int num)
+        {
+            int result = 0;
+            while (true)
+            {
+                if (num != 0)
+                {
+                    if (num % 2 == 0)
+                    {
+                        num = num / 2;
+                        result++;
+                    }
+                    else
+                    {
+                        num -= 1;
+                        result++;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return result;
+        }
     }
 }
