3be83b12770721abcb49c400c13c3f554dfbacfd
SerifeGokdas1 <serifegokdaas@gmail.com>
2024-12-06 19:40:48 +0300

    Generics Problem

---
+++ EasyProblems/GenericsProblem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmChallenges.EasyProblems
+{
+    public class GenericsProblem
+    {
+        public void PrintArray<T>(T[] Array)
+        {
+            foreach(var item in Array) { Console.WriteLine(item); }
+        }
+    }
+}
