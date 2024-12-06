fc2780ca94cde2263b2548cd1927e59936053e3b
SerifeGokdas1 <serifegokdaas@gmail.com>
2024-12-06 15:19:39 +0300

    Time Conversion Problem Solved

---
+++ EasyProblems/TimeConversionProblem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmChallenges.EasyProblems
+{
+    public class TimeConversionProblem
+    {
+        //Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+        //timeConversion has the following parameter(s): string s: a time in 12 hour format, 
+        //Returns: string: the time in 24 hour format
+
+
+        public static string timeConversion(string s)
+        {
+            DateTime newDate = Convert.ToDateTime(s);
+            newDate.AddHours(12);
+            string result = newDate.ToString("HH:mm:ss");
+            return result;
+
+        }
+
+    }
+}
